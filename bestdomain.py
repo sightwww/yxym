6fa51685cf64284d130d468d8ce4ebf200f69e34
sightwww <sightwww@live.com>
2025-12-24 01:20:40 +0800

    Update bestdomain.py

--- bestdomain.py
+++ bestdomain.py
@@ -142,7 +142,7 @@ def main():
     subdomain_ip_mapping = {
         'bestcf': 'https://raw.githubusercontent.com/ymyuuu/IPDB/refs/heads/main/BestCF/bestcfv4.txt',
         'api': 'https://raw.githubusercontent.com/sightwww/yxym/refs/heads/main/ip.txt',
-        'proxyip': 'https://raw.githubusercontent.com/chris202010/yxym/refs/heads/main/proxyip.txt',
+        'proxyip': 'https://raw.githubusercontent.com/sightwww/yxym/refs/heads/main/proxyip.txt',
     }
 
     # 注意：这里的输出现在更准确了，'proxied' 是指 DNS 记录的云朵状态。
